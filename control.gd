5f1bdf4c3fce14690714f364ba50e14cfc6f120f
sayamapp <sanitygames04@gmail.com>
2023-09-03 02:06:05 +0900

    aaaa

--- control.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-var time = 0.0
-
-# Called when the node enters the scene tree for the first time.
-
-
--- sggg.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends SGGGraphPlotter
-
-var time = 0.0
-
-func _process(delta):
-	time += delta * 10.0
-	callable_to_plot = func(x): return cos(x + time) * 10.0
-	plot()
